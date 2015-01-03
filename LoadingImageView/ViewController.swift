0b572aa8162e8b7c6d604dd55a285fd9c44f4108
ggamecrazy <me@cezarcocu.com>
2015-01-03 13:01:11 -0500

    added readme and screenshots

--- LoadingImageView/ViewController.swift
+++ LoadingImageView/ViewController.swift
@@ -12,11 +12,12 @@ class ViewController: UITableViewController {
 
   @IBOutlet weak var loadingImage: LoadingImageView!
   
-  let catURLs = [ NSURL(string: "http://www.mnftiu.cc/wp-content/uploads/2012/12/77669-cats-funny-cat.jpg"),
-  NSURL(string: "http://www.neighborhoodcats.org/uploads/Image/Right%20panel%20images/nycfcc%20site%201.jpg"),
-  NSURL(string: "http://images4.fanpop.com/image/photos/22000000/-_-cats-cats-22066030-1024-768.jpg"),
-  NSURL(string: "http://momusnajmi.files.wordpress.com/2014/12/cat-music.jpg"),
-  NSURL(string: "http://blogs.biomedcentral.com/bmcblog/files/2014/02/Benjamin-Blonder.png")]
+  let catURLs = [ NSURL(string: "http://www.mnftiu.cc/wp-content/uploads/2012/12/77669-cats-funny-cat.jpg")!,
+  NSURL(string: "http://www.neighborhoodcats.org/uploads/Image/Right%20panel%20images/nycfcc%20site%201.jpg")!,
+  NSURL(string: "http://images4.fanpop.com/image/photos/22000000/-_-cats-cats-22066030-1024-768.jpg")!,
+  NSURL(string: "http://momusnajmi.files.wordpress.com/2014/12/cat-music.jpg")!,
+  NSURL(string: "http://blogs.biomedcentral.com/bmcblog/files/2014/02/Benjamin-Blonder.png")!,
+  NSURL(string: "https://catfishes.files.wordpress.com/2013/03/cat-breaded.jpg")!]
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return catURLs.count
@@ -26,7 +27,7 @@ class ViewController: UITableViewController {
     let cell = tableView.dequeueReusableCellWithIdentifier("loadingCell", forIndexPath: indexPath) as LoadingTableViewCell
     
     let imageURL = catURLs[indexPath.row % catURLs.count]
-    cell.configure(imageURL!)
+    cell.configure(imageURL)
     return cell
   }
   override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 LoadingImageView
 ================
 
-Loading Indicator for UIImageView. Handles network calls and background image decoding
+Loading Indicator for UIImageView written in Swift.
+
+Handles network calls and background image decoding.
+
+Handles Error states and retrying download.
+
+![demo](http://i.imgur.com/SpE2BQv.gif)
+
+Code:
+``` swift
+  let imageURL = NSURL(string: "https://catfishes.files.wordpress.com/2013/03/cat-breaded.jpg")
+imageView.downloadImage(imageURL, placeholder: nil)
+```
